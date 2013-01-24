b84d3b004458b8c2cc71691ecb677ad259abedbf
Eric Kansa <ekansa@alexandriaarchive.org>
2013-01-23 20:29:55 -0800

    Add measurement ontology
    Add zooarch measurement ontology, a temporary / testing approach

---
+++ application/controllers/ZooController.php
@@ -0,0 +1,73 @@
+<?php
+
+/** Zend_Controller_Action */
+//require_once 'Zend/Controller/Action.php';
+//require_once 'OpenContext/Controller/Action/Helper/SolrAccess.php';
+
+//error_reporting(E_ALL ^ E_NOTICE);
+// increase the memory limit
+ini_set("memory_limit", "1024M");
+// set maximum execution time to no limit
+ini_set("max_execution_time", "0");
+
+class ZooController extends Zend_Controller_Action {
+    
+    function init()
+    {  
+        require_once 'App/Util/GenericFunctions.php';
+    }
+    
+	 
+	 //load up old space data from XML documents
+	 function addSpaceHierarchyAction(){
+		  
+		  //this line is necessary for ajax calls:
+        $this->_helper->viewRenderer->setNoRender();        
+        
+        //get selected root item then add it and all children to database
+        $baseURL = "http://opencontext/subjects/";
+        $rootUUID = "63431873-B6CB-4F38-5EB2-F667EA2BAF76";
+		  
+		  Zend_Loader::loadClass('PublishedData_Hierarchy');
+        Zend_Loader::loadClass('PublishedData_Space');
+        Zend_Loader::loadClass('PublishedData_Observe');
+		  Zend_Loader::loadClass('PublishedData_Properties');
+		  Zend_Loader::loadClass('PublishedData_Links');
+		  Zend_Loader::loadClass('dbXML_xmlSpace');
+		  
+		  $hierarchyObj = new PublishedData_Hierarchy;
+		  $hierarchyObj->baseSpaceURI = $baseURL;
+		  $hierarchyObj->addHierarchy($rootUUID);
+		  header('Content-Type: application/json; charset=utf8');
+		  echo Zend_Json::encode(array("done" => $hierarchyObj->doneURIs, "errors" => $hierarchyObj->errors));
+	 }
+	 
+
+
+	  //load up old space data from XML documents
+	 function linkBoneOntologyAction(){
+		  
+		  //this line is necessary for ajax calls:
+        $this->_helper->viewRenderer->setNoRender();        
+        
+        //get selected root item then add it and all children to database
+        $tableID = "z_4_aa1c977a0";
+        
+		  Zend_Loader::loadClass('LinkedData_BoneMeasurement');
+		  
+		  $linkingObj = new LinkedData_BoneMeasurement;
+		  $varList = $linkingObj->getVarTableList($tableID);
+		  $doneList = $linkingObj->processVars($varList);
+		 
+		  header('Content-Type: application/json; charset=utf8');
+		  echo Zend_Json::encode($doneList);
+	 }
+	 
+
+	 
+
+
+
+
+
+}//end class
