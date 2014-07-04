4bf94bb440a88619f1bc0698eeb55e5b2bb080cc
Eric Kansa <kansaeric@gmail.com>
2014-07-04 08:17:11 -0700

    Preparing Export to OC-py
    Export data as JSON for easy consumption in the new Open Context Python
    system

---
+++ application/models/PyExport/PyData.php
@@ -0,0 +1,203 @@
+<?php
+/*
+This class edits Murlo data
+
+I'm including it, since it may be useful to adapt to other projects, also it adds a little documentation to my data wrangling
+
+*/
+class PyExport_PyData {
+    
+    public $db;
+    public $requestParams;
+	public $requiredParmams = array("tab", "start", "recs", "after");
+	
+	public $oc_documents = 'oc_documents';
+	public $oc_mediafiles = 'oc_mediafiles';
+	
+	public $oc_manifest = 'oc_manifest';
+	
+	function getData($requestParams){
+		$output = array();
+		$ok = $this->validateRequest($requestParams);
+		if(!$ok){
+			$output['error'] = "Invalid request";
+		}
+		else{
+			if($requestParams['tab'] == $this->oc_documents){
+				//do diaries
+				$output = $this->Docs();
+			}
+			elseif($requestParams['tab'] == $this->oc_mediafiles){
+				//do diaries
+				$output = $this->Media();
+			}
+			
+			
+		}
+		return $output;
+	}
+	
+	
+	function Docs(){
+		$requestParams = $this->requestParams;
+		$db = $this->startDB();
+		$output = array();
+		$output['requestParams'] = $requestParams;
+		$output['tabs'][] = $this->oc_documents;
+		$output['tabs'][] = $this->oc_manifest;
+		$output[$this->oc_documents] = array();
+		$output[$this->oc_manifest] = array();
+		$after = $requestParams["after"];
+		$start = $requestParams["start"];
+		$recs = $requestParams["recs"];
+		
+		$sql = "SELECT * FROM
+		diary
+		WHERE last_modified_timestamp >= '$after'
+		ORDER BY uuid
+		LIMIT $start, $recs
+		";
+		
+		$result =  $db->fetchAll($sql);
+		foreach($result as $row){
+			$doc_rec = array();
+			$doc_rec['uuid'] = $row['uuid'];
+			$doc_rec['project_uuid'] = $row['project_id'];
+			$doc_rec['source_id'] = $row['source_id'];
+			$doc_rec['content'] = $row['diary_text_original'];
+			$sort = $row['sort'];
+			
+			
+			$man_rec = array();
+			$man_rec['uuid'] = $row['uuid'];
+			$man_rec['project_uuid'] = $row['project_id'];
+			$man_rec['source_id'] = $row['source_id'];
+			$man_rec['item_type'] = 'documents';
+			$man_rec['label'] = $row['diary_label'];
+			$man_rec['des_predicate_uuid'] = $this->get_des_predicate($row['uuid']);
+			
+			$output[$this->oc_documents][] = $doc_rec;
+			$output[$this->oc_manifest][] = $man_rec;
+		}
+		return $output;
+	}
+	
+	function Media(){
+		$requestParams = $this->requestParams;
+		$db = $this->startDB();
+		$output = array();
+		$output['requestParams'] = $requestParams;
+		$output['tabs'][] = $this->oc_mediafiles;
+		$output['tabs'][] = $this->oc_manifest;
+		$output[$this->oc_mediafiles] = array();
+		$output[$this->oc_manifest] = array();
+		$after = $requestParams["after"];
+		$start = $requestParams["start"];
+		$recs = $requestParams["recs"];
+		
+		$sql = "SELECT * FROM
+		resource
+		WHERE last_modified_timestamp >= '$after'
+		ORDER BY uuid
+		LIMIT $start, $recs
+		";
+		
+		$result =  $db->fetchAll($sql);
+		foreach($result as $row){
+			$f_rec_b = array();
+			$f_rec_b['uuid'] = $row['uuid'];
+			$f_rec_b['project_uuid'] = $row['project_id'];
+			$f_rec_b['source_id'] = $row['source_id'];
+			
+			$f_rec_t = $f_rec_b;
+			$f_rec_t['file_type'] = 'oc-gen:thumbnail';
+			$f_rec_t['file_uri'] = $row['ia_thumb'];
+			$f_rec_p = $f_rec_b;
+			$f_rec_p['file_type'] = 'oc-gen:preview';
+			$f_rec_p['file_uri'] = $row['ia_preview'];
+			$f_rec_f = $f_rec_b;
+			$f_rec_f['file_type'] = 'oc-gen:fullfile';
+			$f_rec_f['file_uri'] = $row['ia_fullfile'];
+			
+			$sort = $row['res_number'];
+			
+			$man_rec = array();
+			$man_rec['uuid'] = $row['uuid'];
+			$man_rec['project_uuid'] = $row['project_id'];
+			$man_rec['source_id'] = $row['source_id'];
+			$man_rec['item_type'] = 'media';
+			$man_rec['label'] = $row['res_label'];
+			$man_rec['class_uri'] = 'oc-gen:'.$row['res_archml_type'];
+			$man_rec['des_predicate_uuid'] = $this->get_des_predicate($row['uuid']);
+			
+			$output[$this->oc_mediafiles][] = $f_rec_t;
+			$output[$this->oc_mediafiles][] = $f_rec_p;
+			$output[$this->oc_mediafiles][] = $f_rec_f;
+			$output[$this->oc_manifest][] = $man_rec;
+		}
+		return $output;
+	}
+	
+	
+	function get_des_predicate($itemUUID){
+		//get a uuid for a descriptive variable
+		$output = "";
+		$db = $this->startDB();
+		
+		$sql = "SELECT obs.subject_uuid, lo.labelVarUUID
+		FROM observe AS obs
+		JOIN properties AS props ON obs.property_uuid = props.property_uuid
+		JOIN labeling_options AS lo ON lo.labelVarUUID = props.variable_uuid
+		WHERE obs.subject_uuid = '$itemUUID'
+		LIMIT 1;
+		";
+		
+		$result =  $db->fetchAll($sql);
+		if($result){
+			$output = $result[0]['labelVarUUID'];
+		}
+		return $output;
+	}
+	
+	//convert an array into a well-formatted JSON string
+	function JSONoutputString($array){
+		  return json_encode($array, JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
+		  //return json_encode($array, 0);
+	}
+	
+	function validateRequest($requestParams){
+		//makes sure the request is valid
+		$ok = true;
+		foreach($this->requiredParmams as $rparam){
+			if(!array_key_exists($rparam, $requestParams)){
+				$ok = false;
+				break;
+			}
+		}
+		if($ok){
+			$this->requestParams = $requestParams;
+		}
+		return $ok;
+	}
+	
+	function startDB(){ 
+		  if(!$this->db){
+				$db = Zend_Registry::get('db');
+				$this->setUTFconnection($db);
+				$this->db = $db;
+		  }
+		  else{
+				$db = $this->db;
+		  }
+		  
+		  return $db;
+	}
+	 
+	function setUTFconnection($db){
+		  $sql = "SET collation_connection = utf8_unicode_ci;";
+		  $db->query($sql, 2);
+		  $sql = "SET NAMES utf8;";
+		  $db->query($sql, 2);
+    }
+	
+}  
